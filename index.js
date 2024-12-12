2e3fbcfc01fe488dd5d9d5df5854ff43e25d1c44
Edon Malushaj <edon_malushaj@hotmail.com>
2024-12-12 14:31:30 +0100

    changed index.js in feature branch

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ import chalk from "chalk";
 const myUnusedVariable = 'test1'
 myUnusedVariable === 'test2' ? console.log('It works') : console.log('It does not work')
 
-console.log(chalk.blue('It works'))
+console.log(chalk.blue('Making a conflict'))
